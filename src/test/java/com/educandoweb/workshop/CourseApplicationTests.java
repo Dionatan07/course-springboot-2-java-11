b52bf8247a5fe2ba6b966678a1ab510ac3d4ac9a
Dionatan de Andrade <dionatan.andrade@hotmail.com>
2022-08-13 12:10:50 -0300

    Spring Course

---
+++ src/test/java/com/educandoweb/workshop/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
